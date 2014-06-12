c6620817097e3c1fd20a9880a7f36c21c100d712
Brad Ediger <brad.ediger@madriska.com>
2014-06-12 14:54:32 -0500

    Compatibility with RSpec 3 / AS 4
    Closes #1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'rspec/its'
+
 ROOT = File.expand_path('../..', __FILE__)
 Dir[File.join(ROOT, 'spec/support/**/*.rb')].each {|f| require f}
 $LOAD_PATH.unshift(File.expand_path('lib', ROOT))
